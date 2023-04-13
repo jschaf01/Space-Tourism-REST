2404e4f04d3745b086205d99c1f26bc2379a059d
jschaf01 <jschaf01@hotmail.com>
2023-04-12 21:12:09 -0500

    This is the initial push of files

---
+++ Data.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var cors = require('cors')
+var fs = require('fs');
+var path = require('node:path');
+var app = express()
+
+app.use(cors())
+
+app.get('/destination/:name', function (req, res) {
+    const fn = path.join('data.json');
+    fs.readFile( fn, 'utf8', function (err, data) {
+        const destinations = JSON.parse(data).destinations;
+        var destination = destinations.find(x => x.name === req.params.name);
+        if (!destination)
+            destination = destinations[0];
+
+        res.json(destination);
+    });
+})
+
+app.get('/crew/:role', function (req, res) {
+    const fn = path.join('data.json');
+    fs.readFile( fn, 'utf8', function (err, data) {
+        const crew = JSON.parse(data).crew;
+        var member = crew.find(x => x.role === req.params.role);
+        if (!member)
+            member = crew[0];
+
+        res.json(member);
+    });
+})
+
+app.get('/technology/:name', function (req, res) {
+    const fn = path.join('data.json');
+    fs.readFile( fn, 'utf8', function (err, data) {
+        const technologies = JSON.parse(data).technology;
+        var technology = technologies.find(x => x.name === req.params.name);
+        if (!technology)
+        technology = technologies[0];
+
+        res.json(technology);
+    });
+})
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 3000')
+})
+
+
